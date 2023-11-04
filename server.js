c500d492c748096fed732aec488b2160cad9bfb0
juliaeu <juliabenedic13@gmail.com>
2023-11-04 02:31:20 +0000

    all routes

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const express = require("express")
 const app = express()
 
 // destructure properties from environment variables 
-const { PORT = 3000, DATABASE_URL } = process.env
+const { PORT = 3000 } = process.env
 
 // import the mongoose library for the MongoDB database interaction
 const mongoose = require("mongoose")
@@ -41,10 +41,10 @@ const methodOverride = require('method-override')
 ////////////////////////////////
 
 // connect to the MOngoDB database using Mongoose
-mongoose.connect(DATABASE_URL, {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-  });
+// mongoose.connect(DATABASE_URL, {
+//     useUnifiedTopology: true,
+//     useNewUrlParser: true,
+//   });
   // Connection Events
   mongoose.connection
     .on("open", () => console.log("Your are connected to mongoose"))
@@ -55,6 +55,14 @@ mongoose.connect(DATABASE_URL, {
 ////////////////////////////////
 ////////MODELS//////////////////
 ////////////////////////////////
+
+
+const weekSchema = new mongoose.Schema({
+  nameDay: String,
+  description: String,
+  imageURL: String
+})
+const Weekday = mongoose.model("Weekday", weekSchema);
 
 
 ///////////////////////////////
@@ -70,11 +78,81 @@ app.use(express.json())
 // enable custom http methods specified by _method
 app.use(methodOverride('_method'))
 
-// configure cors 
-app.use(corsOptions)
+// // configure cors 
+// app.use(corsOptions)
 
 
 
 /////////////////////////////
 ///////////ROUTES////////
 //////////////////////////////
+app.get("/", (req, res) => {
+    res.send("Hello world")
+  })
+
+
+
+  // READ - Get all weekdays, INDEX ROUTE
+router.get('/weekdays', async (req, res) => {
+    try {
+      const weekdays = await Weekday.find({})
+    } catch (error) {
+      res.status(400).json(error)
+    }
+  })
+
+    
+// CREATE - Add a new weekday 
+router.post('/weekdays', async(req, res) => {
+    try {
+        const {nameDay, description, imageURL} = req.body
+        const newWeekday = await Weekday.create({nameDay, description, imageURL})
+    } catch (error) {
+        res.status(400).json(error)
+    }
+})
+
+
+// READ - get a specific weekday by ID
+router.get('/weekdays/:id', async(req, res) => {
+    try {
+        const weekday = await Weekday.findById(req.params.id)
+        if (!weekday) {
+            return res.status(404).json({error: 'Weekday not found'})
+        }
+    }catch(error){
+        res.status(400).json(error)
+    }
+})
+
+// UPDATE - Update a specific weekday by ID
+router.put('/weekdays/:id', async (req, res) => {
+    try {
+      const updatedWeekday = await Weekday.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        { new: true }
+      )
+      if (!updatedWeekday) {
+        return res.status(404).json({ error: 'Weekday not found' })
+      }
+    }  catch (error) {
+      res.status(400).json(error)
+    }
+  })
+  
+  // DELETE - Delete a specific weekday by ID
+  router.delete('/weekdays/:id', async (req, res) => {
+    try  {
+      const deletedWeekday = await Weekday.findByIdAndRemove(req.params.id)
+      if (!deletedWeekday) {
+        return res.status(404).json({ error: 'Weekday not found' })
+      }
+    } catch (error) {
+      res.status(400).json(error)
+    }
+  })
+  
+
+    // LISTENER
+    app.listen(PORT, () => console.log(`Listening on PORT http://localhost:${PORT}/`));  
